5fcc57a2a0c759797ab4ec37ae7a0d206920a718
LJstudent <poepjan123@gmail.com>
2020-12-23 02:45:36 +0100

    Enter

--- fuels/views.py
+++ fuels/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.def index(request):
+
 def index(request):
-    return render(request, 'fuelsforchristmas/index.html')
+     return render(request, 'fuels/index.html')
