1c3a6a977dbcc81cab8ab58c9bcf425091a94fa6
LJstudent <47226660+LJstudent@users.noreply.github.com>
2020-12-23 03:09:49 +0100

    Update settings.py

--- fuelforchristmas/settings.py
+++ fuelforchristmas/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '31*gl-#&)hik6zz)j#db$on2$f-lcic-e!l9a_qw#h1d36x4cy'
+SECRET_KEY = '////'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
